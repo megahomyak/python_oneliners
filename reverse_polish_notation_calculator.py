41a2dc03ac3ba1d009a5cc2765b5aa3fc202572d
megahomyak <58137927+megahomyak@users.noreply.github.com>
2021-12-13 18:24:19 +0500

    Create reverse_polish_notation_calculator.py

---
+++ reverse_polish_notation_calculator.py
@@ -0,0 +1,65 @@
+(
+    lambda number_regex_pattern, stack: [
+        (
+            [
+                (
+                    print(
+                        f'Unknown word "{minimal_stack_size}" (word number is '
+                        f'{word_number})! Skipping it.'
+                    )
+                    if number_regex_pattern.fullmatch(
+                        minimal_stack_size
+                    ) is None else
+                    stack.append(int(minimal_stack_size))
+                )
+                if operation is None else
+                (
+                    print(
+                        f"Not enough elements on stack to perform the "
+                        f"{operation_name} (word number {word_number})! ("
+                        f"Required: {minimal_stack_size}, stack length: "
+                        f"{len(stack)})"
+                    )
+                    if len(stack) < minimal_stack_size else
+                    stack.append(operation(stack))
+                )
+                for word_number, (
+                    minimal_stack_size, operation, operation_name
+                ) in enumerate(
+                    (
+                        {
+                            "+": (
+                                2, lambda stack: stack.pop() + stack.pop(),
+                                "addition"
+                            ),
+                            "-": (2, lambda stack: (
+                                lambda stack, right_operand: (
+                                    stack.pop() - right_operand
+                                )
+                            )(stack, stack.pop()), "subtraction"),
+                            "*": (
+                                2, lambda stack: stack.pop() * stack.pop(),
+                                "multiplication"
+                            ),
+                            "/": (2, lambda stack: (
+                                lambda stack, right_operand: (
+                                    stack.pop() / right_operand
+                                )
+                            )(stack, stack.pop()), "division")
+                        }.get(word, (word, None, None))
+                        for word in input().split()
+                    ),
+                    start=1
+                )
+            ] == (
+                (
+                    print("Stack top:", stack[-1])
+                    if len(stack) != 0 else
+                    None
+                ) == (
+                    stack.clear()
+                )
+            )
+        ) for _ in __import__("itertools").repeat(1)
+    ]
+)(__import__("re").compile(r"\d+"), [])
